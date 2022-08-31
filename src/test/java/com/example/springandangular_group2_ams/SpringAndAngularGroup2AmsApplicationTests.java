5f4a8da0dda4e0fed661bd5bf89a555a67380108
kimgech <kimgech309@gmail.com>
2022-08-31 16:52:18 +0700

    Initial commit

---
+++ src/test/java/com/example/springandangular_group2_ams/SpringAndAngularGroup2AmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springandangular_group2_ams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndAngularGroup2AmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
