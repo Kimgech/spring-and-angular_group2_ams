be8fd8b587907b5a8c38961537b1d646f08c4028
kimgech <kimgech309@gmail.com>
2022-09-01 15:28:51 +0700

    m

--- src/main/java/com/example/springandangular_group2_ams/model/entities/Comment.java
+++ src/main/java/com/example/springandangular_group2_ams/model/entities/Comment.java
@@ -3,13 +3,14 @@ package com.example.springandangular_group2_ams.model.entities;
 import javax.persistence.*;
 import java.util.UUID;
 
-@Entity(name = "Category")
-@Table(name = "categories")
-public class Category {
+@Entity(name="Comment")
+@Table(name = "comments")
+public class Comment {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private UUID id;
 
-    @Column(name = "name" , length = 100)
-    private String name;
+    @Column(name = "caption")
+    private String caption;
 }
